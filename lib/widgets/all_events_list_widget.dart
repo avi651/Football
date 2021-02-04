a78e8949fdeae3f76aa1492eae3a772834f5cbc5
Avinash <kumaravi651@gmail.com>
2021-02-04 14:47:17 +0530

    Code Committed added animation in the listview for the first screen

--- lib/widgets/all_events_list_widget.dart
+++ lib/widgets/all_events_list_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:football_te/providers/events_provider.dart';
 import 'package:football_te/widgets/friend_attending_widget.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 class AllEventsListWidget extends StatelessWidget {
   @override
@@ -12,20 +13,32 @@ class AllEventsListWidget extends StatelessWidget {
       builder: (ctx, snapshot) =>
           snapshot.connectionState == ConnectionState.waiting
               ? Center(
-                  child: CircularProgressIndicator(),
+                  child: const Text('Fetching the data'),
                 )
               : Consumer<EventProvider>(
                   child: Center(
-                    child: const Text('No and mateches'),
+                    child: const Text('No matches'),
                   ),
                   builder: (ctx, allEventsConsumer, ch) =>
                       allEventsConsumer.allEvents.length <= 0
                           ? ch
-                          : ListView.builder(
-                              itemCount: allEventsConsumer.allEvents.length,
-                              itemBuilder: (ctx, i) => FriendAttendingWidget(
-                                allEventsNetworkImage:
-                                    allEventsConsumer.allEvents[i].mainImage,
+                          : AnimationLimiter(
+                              child: ListView.builder(
+                                itemCount: allEventsConsumer.allEvents.length,
+                                itemBuilder: (ctx, i) =>
+                                    AnimationConfiguration.staggeredList(
+                                  position: i,
+                                  duration: const Duration(milliseconds: 2000),
+                                  child: SlideAnimation(
+                                    verticalOffset: 50.0,
+                                    child: FadeInAnimation(
+                                      child: FriendAttendingWidget(
+                                        allEventsNetworkImage: allEventsConsumer
+                                            .allEvents[i].mainImage,
+                                      ),
+                                    ),
+                                  ),
+                                ),
                               ),
                             ),
                 ),
--- lib/widgets/friend_attending_widget.dart
+++ lib/widgets/friend_attending_widget.dart
@@ -5,17 +5,21 @@ class FriendAttendingWidget extends StatelessWidget {
   FriendAttendingWidget({@required this.allEventsNetworkImage});
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: NetworkImage(allEventsNetworkImage),
-          fit: BoxFit.cover,
+    return Card(
+      elevation: 10,
+      child: Container(
+        height: 200,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          image: DecorationImage(
+            image: NetworkImage(allEventsNetworkImage),
+            fit: BoxFit.cover,
+          ),
         ),
-      ),
-      child: Stack(
-        children: [],
+        child: Column(
+          children: [],
+        ),
       ),
     );
   }
